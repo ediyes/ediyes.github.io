cfe164265dad0ca395b2d74a5ed6ba7e008d59dc
Junior flores <fmjg21@gmail.com>
2024-11-11 17:51:43 -0500

    setup sidebar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-// for rtl format comment out bootstrap.min.css and uncomment bootstrap.rtl.min.css
 import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/css/bootstrap.rtl.min.css";
 
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
@@ -23,7 +20,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
